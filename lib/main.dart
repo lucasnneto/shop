34ce4e491e8f2938da6e48745558d1c435e39f73
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-07-18 23:40:04 -0300

    auth base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop/providers/auth.dart';
+import 'package:shop/views/auth_screen.dart';
 import 'package:shop/views/product_form_screen.dart';
 import 'package:shop/views/products_screen.dart';
 import 'views/products_overview_screen.dart';
@@ -28,6 +30,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => new Orders(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => new Auth(),
+        ),
       ],
       child: MaterialApp(
         title: 'Minha Loja',
@@ -38,6 +43,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: ProductOverviewScreen(),
         routes: {
+          AppRoutes.AUTH: (ctx) => AuthScreen(),
           AppRoutes.HOME: (ctx) => ProductOverviewScreen(),
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
           AppRoutes.CART: (ctx) => CartScreen(),
