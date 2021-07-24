ad60347353d76bed6e69db9b570e1f4342c7f0f5
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-07-24 10:42:42 -0300

    Ignore Scroll- temporary

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -11,6 +11,7 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Container(
