4065153471c2376ce6e614dfa261dd509e8c530e
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-07-25 21:35:48 -0300

    animação e fix height

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -11,7 +11,6 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Container(
@@ -26,37 +25,46 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 60),
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.deepOrange.shade900,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2),
-                        ),
-                      ]),
-                  child: Text(
-                    'Minha Loja',
-                    style: TextStyle(
-                        color:
-                            Theme.of(context).accentTextTheme.headline6!.color,
-                        fontSize: 45,
-                        fontFamily: 'Anton'),
-                  ),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 20), // Gambiarra para corrigir altura
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 60),
+                      transform: Matrix4.rotationZ(-8 * pi / 180)
+                        ..translate(-10.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.deepOrange.shade900,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                            ),
+                          ]),
+                      child: Text(
+                        'Minha Loja',
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .accentTextTheme
+                                .headline6!
+                                .color,
+                            fontSize: 45,
+                            fontFamily: 'Anton'),
+                      ),
+                    ),
+                    AuthCard(),
+                  ],
                 ),
-                AuthCard(),
-              ],
+              ),
             ),
           )
         ],
--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -20,7 +20,8 @@ class _AuthCardState extends State<AuthCard>
   final _passwordController = TextEditingController();
 
   late AnimationController _controller;
-  late Animation<Size> _heightAnimation;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
@@ -31,9 +32,15 @@ class _AuthCardState extends State<AuthCard>
         milliseconds: 300,
       ),
     );
-    _heightAnimation = Tween(
-      begin: Size(double.infinity, 290),
-      end: Size(double.infinity, 371),
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.linear),
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, -1.5),
+      end: Offset(0, 0),
     ).animate(
       CurvedAnimation(parent: _controller, curve: Curves.linear),
     );
@@ -111,8 +118,12 @@ class _AuthCardState extends State<AuthCard>
     return Card(
       elevation: 8,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Login ? 290 : 371,
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16),
         child: Form(
           key: _form,
           child: Column(
@@ -142,20 +153,33 @@ class _AuthCardState extends State<AuthCard>
                 controller: _passwordController,
                 onSaved: (value) => _authData['password'] = value!,
               ),
-              if (_authMode == AuthMode.SignUp)
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
-                  obscureText: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: _authMode == AuthMode.SignUp
-                      ? (value) {
-                          if (value != _passwordController.text) {
-                            return 'Senhas são diferentes';
-                          }
-                          return null;
-                        }
-                      : null,
+              AnimatedContainer(
+                constraints: BoxConstraints(
+                  minHeight: _authMode == AuthMode.SignUp ? 60 : 0,
+                  maxHeight: _authMode == AuthMode.SignUp ? 120 : 0,
                 ),
+                duration: Duration(milliseconds: 300),
+                curve: Curves.linear,
+                child: FadeTransition(
+                  opacity: _opacityAnimation,
+                  child: SlideTransition(
+                    position: _slideAnimation,
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                      obscureText: true,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: _authMode == AuthMode.SignUp
+                          ? (value) {
+                              if (value != _passwordController.text) {
+                                return 'Senhas são diferentes';
+                              }
+                              return null;
+                            }
+                          : null,
+                    ),
+                  ),
+                ),
+              ),
               Spacer(),
               _isLoading
                   ? CircularProgressIndicator()
@@ -180,13 +204,6 @@ class _AuthCardState extends State<AuthCard>
               )
             ],
           ),
-        ),
-        builder: (ctx, child) => Container(
-          height: _heightAnimation.value.height,
-          // height: _authMode == AuthMode.Login ? 290 : 371,
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16),
-          child: child,
         ),
       ),
     );
