7af61dc62c0733d7c01400d91e7dabab23b2afd9
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-07-25 22:09:45 -0300

    Sliver

--- lib/views/product_details_screen.dart
+++ lib/views/product_details_screen.dart
@@ -7,26 +7,46 @@ class ProductDetailScreen extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                  tag: product.id!,
-                  child: Image.network(
-                    product.imageUrl,
-                    fit: BoxFit.cover,
+        // appBar: AppBar(
+        //   title: Text(product.title),
+        // ),
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: product.id!,
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment(0, 0.8),
+                            end: Alignment(0, 0),
+                            colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ])),
+                  ),
+                ],
+              )),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
               SizedBox(height: 10),
               Text(
                 'R\$ ${product.price}',
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.grey,
                   fontSize: 20,
@@ -40,9 +60,12 @@ class ProductDetailScreen extends StatelessWidget {
                   product.description,
                   textAlign: TextAlign.center,
                 ),
-              )
+              ),
+              SizedBox(height: 1000),
             ],
           ),
-        ));
+        )
+      ],
+    ));
   }
 }
