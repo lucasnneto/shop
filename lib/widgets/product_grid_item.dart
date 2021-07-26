099659c803ae99d8676521610952664817e1c6bb
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-07-25 21:48:42 -0300

    image placeholder

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -22,10 +22,17 @@ class ProductGridItem extends StatelessWidget {
               arguments: product,
             );
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(
+              product.imageUrl,
+            ),
             fit: BoxFit.cover,
           ),
+          // Image.network(
+          //   product.imageUrl,
+          //   fit: BoxFit.cover,
+          // ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
